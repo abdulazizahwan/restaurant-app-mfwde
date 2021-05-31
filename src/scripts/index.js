c5a6d1e498c9f2a8524e9f3e892538f8cc4d3e1c
Abdul Aziz Ahwan <abdulazizahwan97@gmail.com>
2021-05-31 20:49:03 +0700

    Fix some suggestion from reviewer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,8 +9,30 @@ window.app = app;
 window.addEventListener('DOMContentLoaded', () => {
 
     document.querySelector("#page-loader").remove();
+    const menuToggle = document.querySelector("#menu-toggle-container");
     const nav = document.querySelector("#nav");
     const mobileNav = document.querySelector(".mobile-nav")
+
+    const menuToggler = () => {
+        const toggler = menuToggle.querySelector("span")
+
+        toggler.classList.contains('active')
+            ? toggler.classList.remove('active')
+                || nav.classList.remove('navbar-list-mobile')
+                || mobileNav.classList.remove('mobile-nav-active')
+            : toggler.classList.add('active')
+                || nav.classList.add('navbar-list-mobile')
+                || mobileNav.classList.add('mobile-nav-active')
+
+    }
+
+    menuToggle.addEventListener('click', menuToggler)
+
+    mobileNav.addEventListener('click',
+        e => e.target !== undefined
+            && e.target.classList.contains('mobile-nav-link')
+            && menuToggler()
+    )
 
     app.renderListRestaurant('#list-restaurant')
 
@@ -22,4 +44,4 @@ window.addEventListener('DOMContentLoaded', () => {
         app.renderListRestaurant('#list-restaurant')
     })
 
-})
+});
