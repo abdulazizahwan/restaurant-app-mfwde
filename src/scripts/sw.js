9eadef7ed914c5b16e5f54c0d26bd7b705a36467
Abdul Aziz Ahwan <abdulazizahwan97@gmail.com>
2021-06-24 11:21:42 +0700

    fix native capability, font awesome 4.7

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -7,7 +7,7 @@ const {
 } = global.serviceWorkerOption;
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(CacheHelper.cachingAppShell([...assets, './'], 'https://use.fontawesome.com'));
+  event.waitUntil(CacheHelper.cachingAppShell([...assets, './']));
 });
 
 self.addEventListener('activate', (event) => {
